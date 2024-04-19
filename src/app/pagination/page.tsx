a04766d50630c5205e7d38b3b017ac8a9f1747c5
Its-Anth0ny <bhattshubhankar27@gmail.com>
2024-04-19 19:43:21 +0530

    major designing

--- src/app/pagination/page.tsx
+++ src/app/pagination/page.tsx
@@ -38,14 +38,16 @@ const ReportsPage = () => {
     }, [rowNum, reports.length]);
 
     return (
-        <div className="">
-            <ReportDialog reports={currentReports} />
-            <Paginator
-                currentPage={currentPage}
-                totalPages={totalPages}
-                onRowNumChange={handleRowNum}
-                onPageChange={handlePageChange}
-            />
+        <div className="w-full h-full flex justify-center items-center bg-gray-200">
+            <div className="h-[80%] max-w-full w-full md:max-w-[75%] md:w-auto lg:max-w-[50%] lg:w-auto flex flex-col justify-between rounded-xl bg-white shadow-2xl">
+                <ReportDialog reports={currentReports} />
+                <Paginator
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onRowNumChange={handleRowNum}
+                    onPageChange={handlePageChange}
+                />
+            </div>
         </div>
     );
 };
--- src/app/pagination/paginator.tsx
+++ src/app/pagination/paginator.tsx
@@ -1,4 +1,5 @@
 import { ChevronFirst, ChevronLast } from "lucide-react";
+import { generateDummyReports } from "../../utils/dummydata";
 
 const Paginator = ({
     currentPage,
@@ -7,50 +8,59 @@ const Paginator = ({
     onPageChange,
 }) => {
     const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+    const reports = generateDummyReports();
 
     return (
-        <div className="paginator">
-            <button
-                onClick={() => {
-                    currentPage > 1 && onPageChange(currentPage - 1);
-                }}
-                disabled={currentPage === currentPage - 1}
-            >
-                <div className="flex">
-                    <ChevronFirst />
-                    <p>Prev</p>
-                </div>
-            </button>
-            {pages.map((page) => (
+        <div className="flex justify-center items-center space-x-14 text-gray-600 text-xs rounded-xl bg-white p-5">
+            <div className="flex items-center">
                 <button
-                    key={page}
-                    onClick={() => onPageChange(page)}
-                    className={currentPage === page ? "active" : ""}
+                    onClick={() => {
+                        currentPage > 1 && onPageChange(currentPage - 1);
+                    }}
+                    disabled={currentPage === currentPage - 1}
                 >
-                    {page}
+                    <div className="flex">
+                        <ChevronFirst />
+                        <p>Prev</p>
+                    </div>
                 </button>
-            ))}
-            <button
-                onClick={() => {
-                    currentPage < totalPages && onPageChange(currentPage + 1);
-                }}
-                disabled={currentPage === currentPage + 1}
-            >
-                <div className="flex">
-                    <p>Next</p>
-                    <ChevronLast />
-                </div>
-            </button>
-            <select
-                onChange={(e) => onRowNumChange(e.target.value)}
-                defaultValue={5}
-            >
-                {[...Array(15)].map((_, index) => (
-                    <option key={index + 1} value={index + 1}>
-                        {index + 1}
-                    </option>
+                {pages.map((page) => (
+                    <button
+                        key={page}
+                        onClick={() => onPageChange(page)}
+                        className={currentPage === page ? "active" : ""}
+                    >
+                        {page}
+                    </button>
                 ))}
-            </select>
+                <button
+                    onClick={() => {
+                        currentPage < totalPages &&
+                            onPageChange(currentPage + 1);
+                    }}
+                    disabled={currentPage === currentPage + 1}
+                >
+                    <div className="flex">
+                        <p>Next</p>
+                        <ChevronLast />
+                    </div>
+                </button>
+            </div>
+            <div>
+                <label>Rows per page: </label>
+                <select
+                    onChange={(e) => onRowNumChange(e.target.value)}
+                    defaultValue={5}
+                >
+                    {[...Array(reports.length < 15 ? reports.length : 15)].map(
+                        (_, index) => (
+                            <option key={index + 1} value={index + 1}>
+                                {index + 1}
+                            </option>
+                        )
+                    )}
+                </select>
+            </div>
         </div>
     );
 };
